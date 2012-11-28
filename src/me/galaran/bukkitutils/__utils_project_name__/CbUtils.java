3c7459f8fa5c51280076730edfecca2d5a142e56
galaran <galaran.h@gmail.com>
2012-11-28 13:16:49 +0400

    Fixed dropStackSafe: ItemStack not always CraftItemStack

--- src/me/galaran/bukkitutils/__utils_project_name__/CbUtils.java
+++ src/me/galaran/bukkitutils/__utils_project_name__/CbUtils.java
@@ -17,7 +17,7 @@ public class CbUtils {
     /** Bukkit's World.dropItem() drops stack with null NBT tag, this method not */
     public static void dropStackSafe(ItemStack stack, Location loc) {
         net.minecraft.server.World nmsWorld = ((CraftWorld) loc.getWorld()).getHandle();
-        net.minecraft.server.ItemStack nmsStack = ((CraftItemStack) stack.clone()).getHandle();
+        net.minecraft.server.ItemStack nmsStack = CraftItemStack.createNMSItemStack(stack);
 
         net.minecraft.server.EntityItem entity =
                 new net.minecraft.server.EntityItem(nmsWorld, loc.getX(), loc.getY(), loc.getZ(), nmsStack);
