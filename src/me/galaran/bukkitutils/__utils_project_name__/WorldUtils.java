55de663a2e5de3a4a2cb2b79fef04905a37f49aa
galaran <galaran.h@gmail.com>
2012-12-29 17:42:49 +0400

    Rewrite player around checker

--- src/me/galaran/bukkitutils/__utils_project_name__/WorldUtils.java
+++ src/me/galaran/bukkitutils/__utils_project_name__/WorldUtils.java
@@ -1,10 +1,27 @@
 package me.galaran.bukkitutils.__utils_project_name__;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 public class WorldUtils {
 
     public static boolean isChunkLoadedAt(Location loc) {
         return loc.getWorld().isChunkLoaded(loc.getBlockX() >> 4, loc.getBlockZ() >> 4);
+    }
+
+    public static boolean isPlayerAround(final Location loc, int radius) {
+        int radiusSquared = radius * radius;
+        
+        Location curPlayerLoc = loc.clone(); // reuse this
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            curPlayerLoc = player.getLocation(curPlayerLoc);
+            if (loc.getWorld().equals(curPlayerLoc.getWorld())) {
+                if (loc.distanceSquared(curPlayerLoc) <= radiusSquared) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
